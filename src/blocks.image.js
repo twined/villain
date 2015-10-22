ab71215c106fa2ba4263d3b9e3dd5245f78f1d8d
Trond Mjøen <trond@twined.net>
2015-10-22 13:50:44 +0200

    Fix bug with multiple image blocks in columns

--- src/blocks.image.js
+++ src/blocks.image.js
@@ -252,7 +252,6 @@ Villain.Blocks.Image = Villain.Block.extend({
 
     setup: function() {
         // check if this block has data. if not, show the setup div
-        that = this;
         if (!this.hasData()) {
             this.$('.villain-image-block').hide();
             $imageDropper = $([
@@ -270,23 +269,14 @@ Villain.Blocks.Image = Villain.Block.extend({
         } else {
             this.clearSetup();
             data = this.getData();
+            $form = $('<form name="image-meta-' + this.dataId + '">');
             $meta = $([
                 '<label for="title">Tittel</label><input value="' + data.title + '" type="text" name="title" />',
                 '<label for="credits">Kreditering</label><input value="' + data.credits + '" type="text" name="credits" />',
                 '<label for="link">URL</label><input value="' + data.link + '" type="text" name="link" />'
             ].join('\n'));
-            this.$setup.append($meta);
-            this.$setup.find('input[name="title"]').on('keyup', _.debounce(function (e) {
-                that.setDataProperty('title', $(this).val());
-            }, 700, false));
-            this.$setup.find('input[name="credits"]').on('keyup', _.debounce(function (e) {
-                that.setDataProperty('credits', $(this).val());
-            }, 700, false));
-            this.$setup.find('input[name="link"]').on('keyup', _.debounce(function (e) {
-                that.setDataProperty('link', $(this).val());
-            }, 700, false));
-
-            this.$setup.append($('<label>Størrelse</label>'));
+            $form.append($meta);
+            $form.append($('<label>Størrelse</label>'));
 
             /* create sizes overview */
             for (var key in data.sizes) {
@@ -296,12 +286,23 @@ Villain.Blocks.Image = Villain.Block.extend({
                         checked = ' checked="checked"';
                     }
                     $radio = $('<label for="' + key +'">'
-                           + '<input type="radio" name="' + 'imagesize'
-                           + '" value="' + data.sizes[key] + '"'
+                           + '<input type="radio" name="imagesize" '
+                           + 'value="' + data.sizes[key] + '"'
                            + checked + ' />' + key + '</label>');
-                    this.$setup.append($radio);
+                    $form.append($radio);
                 }
             }
+
+            this.$setup.append($form);
+            this.$setup.find('input[name="title"]').on('keyup', _.debounce($.proxy(function (e) {
+                this.setDataProperty('title', $(e.target).val());
+            }, this), 700, false));
+            this.$setup.find('input[name="credits"]').on('keyup', _.debounce($.proxy(function (e) {
+                this.setDataProperty('credits', $(e.target).val());
+            }, this), 700, false));
+            this.$setup.find('input[name="link"]').on('keyup', _.debounce($.proxy(function (e) {
+                this.setDataProperty('link', $(e.target).val());
+            }, this), 700, false));
             this.$setup.find('input[type=radio]').on('change', $.proxy(function(e) {
                 this.setUrl($(e.target).val());
             }, this));
