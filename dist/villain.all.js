f2d0d677268b9bf84c7b9772bf80126a0a0838be
Trond Mjøen <trond@twined.net>
2016-09-16 23:19:44 +0200

    Support nested columns

--- dist/villain.all.js
+++ dist/villain.all.js
@@ -4756,8 +4756,6 @@ if (typeof exports === 'object') {
     
             anchor = data['anchor'] || '';
     
-            console.log(anchor);
-    
             this.$setup.hide();
             var radios = "";
             levels = [1, 2, 3, 4, 5];
@@ -5980,13 +5978,10 @@ if (typeof exports === 'object') {
     
     Villain.Blocks.Columns = Villain.Block.extend({
         type: 'columns',
-        template: _.template('<div class="row"></div>'),
+        template: _.template('<div id="villain-column-row-<%= columnId %>" class="row"></div>'),
         columnTemplate: _.template('<div class="<%= columnClass %>"></div>'),
     
-        events: {
-            'keyup input[name="villain-columns-number"]': '_updateColumnCount',
-            'click button.villain-columns-apply': '_applyColumnCount'
-        },
+        events: {},
     
         initialize: function(json, store) {
             Villain.Block.prototype.initialize.apply(this, [json, store]);
@@ -6023,7 +6018,7 @@ if (typeof exports === 'object') {
             this.store = this.id;
             this.$el.attr('data-blockstore', this.store);
     
-            blockTemplate = this.template({content: this.data});
+            blockTemplate = this.template({columnId: this.dataId});
             actionsTemplate = this.actionsTemplate();
             wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
             this.el.innerHTML = wrapperTemplate;
@@ -6051,17 +6046,19 @@ if (typeof exports === 'object') {
         },
     
         renderEditorHtml: function() {
-            this.parseRow();
-            blockTemplate = this.template({content: this.data});
-            actionsTemplate = this.actionsTemplate();
-            wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+            this.parseRow(this.getRow());
+            var blockTemplate = this.template({columnId: this.dataId}),
+                actionsTemplate = this.actionsTemplate(),
+                wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+    
             return this;
         },
     
         renderEmpty: function() {
-            blockTemplate = this.template({content: this.data});
-            actionsTemplate = this.actionsTemplate();
-            wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+            var blockTemplate = this.template({columnId: this.dataId}),
+                actionsTemplate = this.actionsTemplate(),
+                wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+    
             return this;
         },
     
@@ -6069,67 +6066,111 @@ if (typeof exports === 'object') {
             if (this.$row) {
                 return this.$row;
             }
-            this.$row = this.$('.row');
+            this.$row = this.$('#villain-column-row-' + this.dataId);
             return this.$row;
         },
     
         getColumns: function(filter) {
-          return this.getRow().children(filter);
+            return this.getRow().children(filter);
         },
     
         getColumn: function(index) {
-          return this.getRow().children(':eq(' + index + ')');
+            return this.getRow().children(':eq(' + index + ')');
         },
     
-        parseRow: function() {
+        parseRow: function($row) {
             // create the columns
-            $row = this.getRow();
-            for (var i = 0; i <= this.data.length - 1; i++) {
-                columnClass = this.data[i].class;
-                columnData = this.data[i].data;
-                $column = $('<div class="' + columnClass + '"></div>');
+            return this.createColumns($row, this.data, this.store);
+        },
+    
+        createColumns: function($row, data, store) {
+            for (var i = 0; i <= data.length - 1; i++) {
+                var addblock = new Villain.Plus(store),
+                    columnClass = data[i].class,
+                    columnData = data[i].data,
+                    $column = $('<div class="' + columnClass + '"></div>');
+    
                 $row.append($column);
-                $column = this.getColumn(i);
-                addblock = new Villain.Plus(this.store);
+                $column = $row.children(':eq(' + i + ')');
                 $column.append(addblock.$el);
+    
                 for (var j = 0; j < columnData.length; j++) {
                     if ((BlockClass = Villain.BlockRegistry.getBlockClassByType(columnData[j].type)) !== false) {
-                        block = new BlockClass(columnData[j].data, this.store);
+                        var block = new BlockClass(columnData[j].data, store);
                         $column.append(block.$el);
-                        addblock = new Villain.Plus(this.store);
+    
+                        addblock = new Villain.Plus(store);
                         $column.append(addblock.$el);
                     }
                 }
             }
+    
+            return $row;
         },
     
         getJSON: function() {
+            return this.parseColumns(this.getColumns());
+        },
+    
+        parseColumns: function(columns) {
+            var _this = this;
             var json = {
                 type: this.type,
                 data: []
             };
-            this.getColumns().each(function(i) {
-                var blocksData = [];
-                $(this).children('.villain-block-wrapper').each(function() {
-                    var block = Villain.BlockStore.getBlockById(
-                        $(this).attr('data-blockstore'), $(this).attr('data-block-id'));
-                    blocksData.push(block.getJSON());
-                });
+    
+            columns.each(function() {
+                var blocksData = _this.parseBlocks(this);
+    
                 json.data.push({'class': $(this).attr('class'), data: blocksData});
             });
     
             return json;
         },
     
+        parseBlocks: function(blocks) {
+            var blocksData = [];
+            var _this = this;
+    
+            $(blocks).children('.villain-block-wrapper').each(function() {
+                // loop through all blocks inside this column
+                var json = _this.parseBlock(this);
+                blocksData.push(json);
+            });
+    
+            return blocksData;
+        },
+    
+        parseBlock: function(block) {
+            if ($(block).attr('data-block-type') == 'columns') {
+                var id = $(block).attr('data-block-id');
+                var subCols = $('#villain-column-row-' + id, $(block)).children();
+                return this.parseColumns(subCols);
+            }
+    
+            var blockStore = $(block).attr('data-blockstore'),
+                blockId = $(block).attr('data-block-id'),
+                blockObj = Villain.BlockStore.getBlockById(blockStore, blockId),
+                blockJson = blockObj.getJSON();
+    
+            return blockJson;
+        },
+    
+        parseCol: function(i) {},
+    
         setup: function() {
             // check if this block has data
             // if not, show the setup div
+            var _this = this;
             if (!this.hasData()) {
                 this.getRow().hide();
                 this.$setup.append([
                     '<label for="villain-columns-number">Antall kolonner</label>',
-                    '<input type="text" class="villain-columns-number" name="villain-columns-number" />'
+                    '<input type="text" id="villain-columns-number-' + this.dataId +  '" class="villain-columns-number" name="villain-columns-number" />'
                 ].join('\n'));
+    
+                this.$setup.on('keyup', 'input#villain-columns-number-' + this.dataId, function(e) {_this.updateColumnCount(e)});
+    
                 this.$setup.show();
                 this.$('.villain-columns-number').attr('autofocus', 'autofocus');
             } else {
@@ -6137,10 +6178,12 @@ if (typeof exports === 'object') {
             }
         },
     
-        _updateColumnCount: function(e) {
+        updateColumnCount: function(e) {
+            var _this = this;
             var columnCount = $(e.target).val();
             this.$('.villain-column-widths').remove();
             columnCountWrapper = $('<div class="villain-column-widths" />');
+    
             for (var i = 1; i < (parseInt(columnCount) + 1); i++) {
                 columnCountWrapper.append([
                     '<label for="villain-column-width-' + i + '">' +
@@ -6148,14 +6191,15 @@ if (typeof exports === 'object') {
                     '<input type="text" name="villain-column-width-' + i + '" class="villain-column-width" />'
                 ].join('\n'));
             }
-            columnCountWrapper.append('<button class="villain-columns-apply">Sett opp kolonner</button>');
+            columnCountWrapper.append('<button id="villain-columns-apply-' + this.dataId + '" class="villain-columns-apply">Sett opp kolonner</button>');
             this.$setup.append(columnCountWrapper);
+            this.$setup.on('click', 'button#villain-columns-apply-' + this.dataId, function(e) {_this.applyColumnCount(e)});
     
         },
     
-        _applyColumnCount: function(e) {
+        applyColumnCount: function(e) {
             e.preventDefault();
-            columnCount = this.$('input[name="villain-columns-number"]').val();
+            columnCount = this.$('input#villain-columns-number-' + this.dataId).val();
             for (var i = 1; i < (parseInt(columnCount) + 1); i++) {
                 columnClass = this.$('input[name="villain-column-width-' + i + '"]').val();
                 this.getRow().append(this.columnTemplate({columnClass: columnClass}));
@@ -6187,6 +6231,7 @@ if (typeof exports === 'object') {
             return addblock;
         }
     },
+    
     {
         /* Static methods */
         getButton: function(afterId) {
--- src/blocks.columns.js
+++ src/blocks.columns.js
@@ -2,13 +2,10 @@
 
 Villain.Blocks.Columns = Villain.Block.extend({
     type: 'columns',
-    template: _.template('<div class="row"></div>'),
+    template: _.template('<div id="villain-column-row-<%= columnId %>" class="row"></div>'),
     columnTemplate: _.template('<div class="<%= columnClass %>"></div>'),
 
-    events: {
-        'keyup input[name="villain-columns-number"]': '_updateColumnCount',
-        'click button.villain-columns-apply': '_applyColumnCount'
-    },
+    events: {},
 
     initialize: function(json, store) {
         Villain.Block.prototype.initialize.apply(this, [json, store]);
@@ -45,7 +42,7 @@ Villain.Blocks.Columns = Villain.Block.extend({
         this.store = this.id;
         this.$el.attr('data-blockstore', this.store);
 
-        blockTemplate = this.template({content: this.data});
+        blockTemplate = this.template({columnId: this.dataId});
         actionsTemplate = this.actionsTemplate();
         wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
         this.el.innerHTML = wrapperTemplate;
@@ -73,17 +70,19 @@ Villain.Blocks.Columns = Villain.Block.extend({
     },
 
     renderEditorHtml: function() {
-        this.parseRow();
-        blockTemplate = this.template({content: this.data});
-        actionsTemplate = this.actionsTemplate();
-        wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+        this.parseRow(this.getRow());
+        var blockTemplate = this.template({columnId: this.dataId}),
+            actionsTemplate = this.actionsTemplate(),
+            wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+
         return this;
     },
 
     renderEmpty: function() {
-        blockTemplate = this.template({content: this.data});
-        actionsTemplate = this.actionsTemplate();
-        wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+        var blockTemplate = this.template({columnId: this.dataId}),
+            actionsTemplate = this.actionsTemplate(),
+            wrapperTemplate = this.wrapperTemplate({content: blockTemplate, actions: actionsTemplate});
+
         return this;
     },
 
@@ -91,67 +90,111 @@ Villain.Blocks.Columns = Villain.Block.extend({
         if (this.$row) {
             return this.$row;
         }
-        this.$row = this.$('.row');
+        this.$row = this.$('#villain-column-row-' + this.dataId);
         return this.$row;
     },
 
     getColumns: function(filter) {
-      return this.getRow().children(filter);
+        return this.getRow().children(filter);
     },
 
     getColumn: function(index) {
-      return this.getRow().children(':eq(' + index + ')');
+        return this.getRow().children(':eq(' + index + ')');
     },
 
-    parseRow: function() {
+    parseRow: function($row) {
         // create the columns
-        $row = this.getRow();
-        for (var i = 0; i <= this.data.length - 1; i++) {
-            columnClass = this.data[i].class;
-            columnData = this.data[i].data;
-            $column = $('<div class="' + columnClass + '"></div>');
+        return this.createColumns($row, this.data, this.store);
+    },
+
+    createColumns: function($row, data, store) {
+        for (var i = 0; i <= data.length - 1; i++) {
+            var addblock = new Villain.Plus(store),
+                columnClass = data[i].class,
+                columnData = data[i].data,
+                $column = $('<div class="' + columnClass + '"></div>');
+
             $row.append($column);
-            $column = this.getColumn(i);
-            addblock = new Villain.Plus(this.store);
+            $column = $row.children(':eq(' + i + ')');
             $column.append(addblock.$el);
+
             for (var j = 0; j < columnData.length; j++) {
                 if ((BlockClass = Villain.BlockRegistry.getBlockClassByType(columnData[j].type)) !== false) {
-                    block = new BlockClass(columnData[j].data, this.store);
+                    var block = new BlockClass(columnData[j].data, store);
                     $column.append(block.$el);
-                    addblock = new Villain.Plus(this.store);
+
+                    addblock = new Villain.Plus(store);
                     $column.append(addblock.$el);
                 }
             }
         }
+
+        return $row;
     },
 
     getJSON: function() {
+        return this.parseColumns(this.getColumns());
+    },
+
+    parseColumns: function(columns) {
+        var _this = this;
         var json = {
             type: this.type,
             data: []
         };
-        this.getColumns().each(function(i) {
-            var blocksData = [];
-            $(this).children('.villain-block-wrapper').each(function() {
-                var block = Villain.BlockStore.getBlockById(
-                    $(this).attr('data-blockstore'), $(this).attr('data-block-id'));
-                blocksData.push(block.getJSON());
-            });
+
+        columns.each(function() {
+            var blocksData = _this.parseBlocks(this);
+
             json.data.push({'class': $(this).attr('class'), data: blocksData});
         });
 
         return json;
     },
 
+    parseBlocks: function(blocks) {
+        var blocksData = [];
+        var _this = this;
+
+        $(blocks).children('.villain-block-wrapper').each(function() {
+            // loop through all blocks inside this column
+            var json = _this.parseBlock(this);
+            blocksData.push(json);
+        });
+
+        return blocksData;
+    },
+
+    parseBlock: function(block) {
+        if ($(block).attr('data-block-type') == 'columns') {
+            var id = $(block).attr('data-block-id');
+            var subCols = $('#villain-column-row-' + id, $(block)).children();
+            return this.parseColumns(subCols);
+        }
+
+        var blockStore = $(block).attr('data-blockstore'),
+            blockId = $(block).attr('data-block-id'),
+            blockObj = Villain.BlockStore.getBlockById(blockStore, blockId),
+            blockJson = blockObj.getJSON();
+
+        return blockJson;
+    },
+
+    parseCol: function(i) {},
+
     setup: function() {
         // check if this block has data
         // if not, show the setup div
+        var _this = this;
         if (!this.hasData()) {
             this.getRow().hide();
             this.$setup.append([
                 '<label for="villain-columns-number">Antall kolonner</label>',
-                '<input type="text" class="villain-columns-number" name="villain-columns-number" />'
+                '<input type="text" id="villain-columns-number-' + this.dataId +  '" class="villain-columns-number" name="villain-columns-number" />'
             ].join('\n'));
+
+            this.$setup.on('keyup', 'input#villain-columns-number-' + this.dataId, function(e) {_this.updateColumnCount(e)});
+
             this.$setup.show();
             this.$('.villain-columns-number').attr('autofocus', 'autofocus');
         } else {
@@ -159,10 +202,12 @@ Villain.Blocks.Columns = Villain.Block.extend({
         }
     },
 
-    _updateColumnCount: function(e) {
+    updateColumnCount: function(e) {
+        var _this = this;
         var columnCount = $(e.target).val();
         this.$('.villain-column-widths').remove();
         columnCountWrapper = $('<div class="villain-column-widths" />');
+
         for (var i = 1; i < (parseInt(columnCount) + 1); i++) {
             columnCountWrapper.append([
                 '<label for="villain-column-width-' + i + '">' +
@@ -170,14 +215,15 @@ Villain.Blocks.Columns = Villain.Block.extend({
                 '<input type="text" name="villain-column-width-' + i + '" class="villain-column-width" />'
             ].join('\n'));
         }
-        columnCountWrapper.append('<button class="villain-columns-apply">Sett opp kolonner</button>');
+        columnCountWrapper.append('<button id="villain-columns-apply-' + this.dataId + '" class="villain-columns-apply">Sett opp kolonner</button>');
         this.$setup.append(columnCountWrapper);
+        this.$setup.on('click', 'button#villain-columns-apply-' + this.dataId, function(e) {_this.applyColumnCount(e)});
 
     },
 
-    _applyColumnCount: function(e) {
+    applyColumnCount: function(e) {
         e.preventDefault();
-        columnCount = this.$('input[name="villain-columns-number"]').val();
+        columnCount = this.$('input#villain-columns-number-' + this.dataId).val();
         for (var i = 1; i < (parseInt(columnCount) + 1); i++) {
             columnClass = this.$('input[name="villain-column-width-' + i + '"]').val();
             this.getRow().append(this.columnTemplate({columnClass: columnClass}));
