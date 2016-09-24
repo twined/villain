486900a5f3e5b3c0b76b35c429e4318c8fc9e588
Trond Mjøen <trond@twined.net>
2016-09-24 20:17:50 +0200

    Build with uid

--- public/villain.all.js
+++ public/villain.all.js
@@ -15990,6 +15990,29 @@ module.exports = [
   })();
 });
 
+require.register("uid/index.js", function(exports, require, module) {
+  require = __makeRelativeRequire(require, {}, "uid");
+  (function() {
+    /**
+ * Export `uid`
+ */
+
+module.exports = uid;
+
+/**
+ * Create a `uid`
+ *
+ * @param {String} len
+ * @return {String} uid
+ */
+
+function uid(len) {
+  len = len || 7;
+  return Math.random().toString(35).substr(2, len);
+}
+  })();
+});
+
 require.register("underscore/underscore.js", function(exports, require, module) {
   require = __makeRelativeRequire(require, {}, "underscore");
   (function() {
